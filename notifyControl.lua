e022f867ae9be55487c547a1535849b341a0cd21
Obstronomic <96883948+Obstronomic@users.noreply.github.com>
2022-09-08 14:28:41 +0200

    notifyControl patch v573

--- notifyControl.lua
+++ notifyControl.lua
@@ -1,128 +1,177 @@
 
 
-local notifySystem = Instance.new("ScreenGui")
-local OKNotify = Instance.new("Frame")
+
+
+local NotifyControl = Instance.new("ScreenGui")
+local NotifyMain = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
 local separao = Instance.new("TextLabel")
-local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
+
 local title = Instance.new("TextLabel")
-local UITextSizeConstraint_2 = Instance.new("UITextSizeConstraint")
+
 local desc = Instance.new("TextLabel")
-local UITextSizeConstraint_3 = Instance.new("UITextSizeConstraint")
+
 local okButton = Instance.new("TextButton")
 local UICorner_2 = Instance.new("UICorner")
-local UITextSizeConstraint_4 = Instance.new("UITextSizeConstraint")
-local UIBlur = Instance.new("BlurEffect", game.Lighting)
 
 
-notifySystem.Name = "notifySystem"
-notifySystem.Parent = game.CoreGui
-notifySystem.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-OKNotify.Name = "OKNotify"
-OKNotify.Parent = notifySystem
-OKNotify.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
-OKNotify.Position = UDim2.new(0.284424365, 0, 0.380952388, 0)
-OKNotify.Size = UDim2.new(0.443023264, 0, 0.236507937, 0)
-OKNotify.Visible = false
+NotifyControl.Name = "NotifyControl"
+NotifyControl.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+NotifyControl.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-UIBlur.Size = 0
+NotifyMain.Name = "NotifyMain"
+NotifyMain.Parent = NotifyControl
+NotifyMain.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
+NotifyMain.Position = UDim2.new(0.00694638537, 0, 0.0111111104, 0)
+NotifyMain.Size = UDim2.new(0, 270, 0, 124)
 
-UICorner.Parent = OKNotify
+UICorner.CornerRadius = UDim.new(0, 3)
+UICorner.Parent = NotifyMain
 
 separao.Name = "separao"
-separao.Parent = OKNotify
-separao.BackgroundColor3 = Color3.fromRGB(255, 0, 127)
+separao.Parent = NotifyMain
+separao.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 separao.BorderColor3 = Color3.fromRGB(255, 0, 127)
-separao.Position = UDim2.new(0, 0, 0.194630876, 0)
-separao.Size = UDim2.new(1, 0, 0, 0)
+separao.Position = UDim2.new(0, 0, 0.169354841, 0)
+separao.Size = UDim2.new(0, 270, 0, 0)
 separao.Font = Enum.Font.SourceSans
+separao.Text = " "
 separao.TextColor3 = Color3.fromRGB(0, 0, 0)
-separao.TextScaled = true
-separao.TextSize = 1.000
-separao.TextTransparency = 1.000
-separao.TextWrapped = true
+separao.TextSize = 14.000
 
-UITextSizeConstraint.Parent = separao
-UITextSizeConstraint.MaxTextSize = 1
+
 
 title.Name = "title"
-title.Parent = OKNotify
-title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+title.Parent = NotifyMain
+title.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
 title.BackgroundTransparency = 1.000
-title.Size = UDim2.new(1, 0, 0.194630876, 0)
+title.Position = UDim2.new(0.025925925, 0, 0, 0)
+title.Size = UDim2.new(0, 193, 0, 21)
 title.Font = Enum.Font.Code
-title.Text = "no title assigned"
+title.Text = "COD"
 title.TextColor3 = Color3.fromRGB(255, 255, 255)
-title.TextScaled = true
 title.TextSize = 14.000
-title.TextWrapped = true
+title.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+title.TextXAlignment = Enum.TextXAlignment.Left
 
-UITextSizeConstraint_2.Parent = title
-UITextSizeConstraint_2.MaxTextSize = 14
+
 
 desc.Name = "desc"
-desc.Parent = OKNotify
-desc.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+desc.Parent = NotifyMain
+desc.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
 desc.BackgroundTransparency = 1.000
-desc.Position = UDim2.new(0.0183727033, 0, 0.241610736, 0)
-desc.Size = UDim2.new(0.965879261, 0, 0.57718122, 0)
+desc.Position = UDim2.new(0.0111111114, 0, 0.217741936, 0)
+desc.Size = UDim2.new(0, 261, 0, 72)
 desc.Font = Enum.Font.Code
-desc.TextColor3 = Color3.fromRGB(255, 255, 255)
-desc.TextScaled = true
+desc.TextColor3 = Color3.fromRGB(255, 0, 127)
 desc.TextSize = 14.000
 desc.TextWrapped = true
 desc.TextXAlignment = Enum.TextXAlignment.Left
 desc.TextYAlignment = Enum.TextYAlignment.Top
 
-UITextSizeConstraint_3.Parent = desc
-UITextSizeConstraint_3.MaxTextSize = 14
+
 
 okButton.Name = "okButton"
-okButton.Parent = OKNotify
+okButton.Parent = NotifyMain
 okButton.BackgroundColor3 = Color3.fromRGB(255, 0, 127)
-okButton.Position = UDim2.new(0.682414711, 0, 0.818791926, 0)
-okButton.Size = UDim2.new(0.301837265, 0, 0.140939593, 0)
-okButton.Font = Enum.Font.Code
-okButton.Text = "OK."
+okButton.Position = UDim2.new(0.688888907, 0, 0.79838711, 0)
+okButton.Size = UDim2.new(0, 78, 0, 18)
+okButton.Font = Enum.Font.SourceSans
 okButton.TextColor3 = Color3.fromRGB(0, 0, 0)
-okButton.TextScaled = true
 okButton.TextSize = 14.000
-okButton.TextWrapped = true
 
-UICorner_2.CornerRadius = UDim.new(0, 4)
+UICorner_2.CornerRadius = UDim.new(0, 3)
 UICorner_2.Parent = okButton
 
-UITextSizeConstraint_4.Parent = okButton
-UITextSizeConstraint_4.MaxTextSize = 14
 
 
+UICorner_2.CornerRadius = UDim.new(0, 3)
+UICorner_2.Parent = okButton
+
+UISTROK = Instance.new("UIStroke")
+
+UISTROK.Parent = NotifyMain
+UISTROK.Color = Color3.fromRGB(255, 0, 127)
+UISTROK.Thickness = 1
 
 
------------------------------
------------------------------
+okButton.Text = "OK."
+okButton.Font = Enum.Font.Code
 
-local A_B = game.PlaceVersion
-print(A_B)
-local RBXL_CLIENT_PATCH_VERSION = "version-d3bea54eff824cef"
-local sound = Instance.new("Sound", game.Workspace)
-sound.SoundId = "rbxassetid://654933978"
-sound.Volume = 1
+local speed = 0.01
 
-
-function makeNotify_Outside(title_Text: any ,text: any, N: any)
-	if N == "isBad_asFuck" then
-		game:Shutdown()
-	elseif N then
-		UIBlur.Size = 24
-		OKNotify.Visible = true
-		sound:Play()
-		desc.Text = text
-		title.Text = title_Text
-		okButton.MouseButton1Down:Connect(function()
-			UIBlur.Size = 0
-			OKNotify.Visible = false
-		end)
-	end
+function gtSound(Play) -- Play function
+	local sound = Instance.new("Sound", game.Workspace)
+	sound.SoundId = "rbxassetid://654933978"
+	sound.Volume = 5
+	sound:Play()
 end
 
+function notify(txt: any, dec: any) -- NOT OPEN {-0.275, -10},{0.014, -2}, OPEN {-0.275, 279},{0.014, -2}
+	title.Text = txt
+	desc.Text = dec
+	okButton.Visible = true
+	NotifyMain.Visible = true
+	gtSound()
+	okButton.MouseButton1Click:Connect(function()
+		okButton.Visible = false
+		NotifyMain.BackgroundTransparency = 0.1
+		separao.BackgroundTransparency = 0.1
+		wait(speed)
+
+
+		NotifyMain.BackgroundTransparency = 0.2
+		separao.BackgroundTransparency = 0.2
+		wait(speed)
+
+
+		NotifyMain.BackgroundTransparency = 0.3
+		separao.BackgroundTransparency = 0.3
+		wait(speed)
+
+
+		NotifyMain.BackgroundTransparency = 0.4
+		separao.BackgroundTransparency = 0.4
+		wait(speed)
+
+
+		NotifyMain.BackgroundTransparency = 0.5
+		separao.BackgroundTransparency = 0.5
+		wait(speed)
+
+
+		NotifyMain.BackgroundTransparency = 0.6
+		separao.BackgroundTransparency = 0.6
+		wait(speed)
+
+
+		NotifyMain.BackgroundTransparency = 0.7
+		separao.BackgroundTransparency = 0.7
+		wait(speed)
+
+
+		NotifyMain.BackgroundTransparency = 0.8
+		separao.BackgroundTransparency = 0.8
+		wait(speed)
+
+
+		NotifyMain.BackgroundTransparency = 0.9
+		separao.BackgroundTransparency = 0.9
+		wait(speed)
+
+
+		NotifyMain.BackgroundTransparency = 1
+		separao.BackgroundTransparency = 1
+	
+		NotifyMain.Visible = false
+		NotifyMain.BackgroundTransparency = 0
+		desc.BackgroundTransparency = 0
+		title.BackgroundTransparency = 0
+		separao.BackgroundTransparency = 0
+	end)
+end
+
+function testNotify()
+	notify("Hello worl", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+end
