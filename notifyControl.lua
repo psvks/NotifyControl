932b0192fcc921bda2d14530267539628a11b793
psvks <113241132+psvks@users.noreply.github.com>
2022-09-17 11:54:54 +0200

    Update notifyControl.lua

--- notifyControl.lua
+++ notifyControl.lua
@@ -1,7 +1,9 @@
 -- Powered by SNUB Hook optimizer.
 -- Latest version of SNUB [{verison]
 
-loadstring(game:HttpGetAsync("https://udkpg.github.io/snub/snub_version/snub_386.lua"))()
+loadstring(game:HttpGetAsync("https://udkpg.github.io/snub/snub_version/snub_386.lua"))() -- update for 386
+
+
 
 if NotiFyControlLoaded and not _G.NotifyControl == true then
         -- err("Can't execute ERR: 847 Please check the wiki for info")
